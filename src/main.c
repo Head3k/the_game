143d1f7788c5becdc14b5c3143eb662126003658
Anibal Woolfield <awoolfie@ob-d9.nsk.21-school.ru>
2021-08-04 18:44:02 +0700

    Add enter string

--- src/main.c
+++ src/main.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 
-int input(int enter);
+#define NMAX 550
+
+char input(char *enter);
 
 int main() {
-	int enter = 0;
-	enter = input(enter);
-	printf("%d", enter);
+	char enter[NMAX];
+	input(enter);
+	printf("%s", enter);
 	return 0;
 }
 
-int input(int enter) {
+char input(char *enter) {
 	int check_i = 0;
-	char check_c = ' ';
-	check_i = scanf("%d%c", &enter, &check_c);
-	return 0;
+	//char check_c = ' ';
+	check_i = scanf("%s", enter);
+	return *enter;
 }
