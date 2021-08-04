237d18ee1f12bb2765be8a60eb026312c4be20b0
Anibal Woolfield <awoolfie@ob-d9.nsk.21-school.ru>
2021-08-04 18:20:44 +0700

    Add new files

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int input(int enter);
+
+int main() {
+	int enter = 0;
+	enter = input(enter);
+	return 0;
+}
+
+int input(int enter) {
+	int check_i = 0;
+	char check_c ' ';
+	check_i = scanf("%d%c", &enter, &check_c);
+	checker(check_i, check_c);
+}
+
+void checker(int check_i, char check_c) {
+	if (check_i != 2 || )
+}
